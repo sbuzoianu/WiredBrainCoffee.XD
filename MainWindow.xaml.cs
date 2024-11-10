3a87b96a0bde9ab91d6bfbe819e1c86ad631fd61
Stefan Buzoianu <stefan.buzoianu@napa.fi>
2024-11-10 19:01:28 +0200

    Acess attached properties with static methods

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -19,7 +19,11 @@ namespace WiredBrainCoffee {
         }
 
         private void ButtonMoveNavigation_Click(object sender, RoutedEventArgs e) {
-            var column = (int)customerListGrid.GetValue(Grid.ColumnProperty);
+            //var column = (int)customerListGrid.GetValue(Grid.ColumnProperty);
+            //var newColumn = column == 0 ? 2 : 0;
+            //customerListGrid.SetValue(Grid.ColumnProperty, newColumn);
+
+            var column = Grid.GetColumn(customerListGrid);
 
             var newColumn = column == 0 ? 2 : 0;
 
