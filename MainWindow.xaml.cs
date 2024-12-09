f179ab54b1da9e83374f91cb334dd0ad564d3a35
Stefan Buzoianu <stefan.buzoianu@napa.fi>
2024-12-09 06:47:05 +0200

    Create a UserControl for Customers

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,16 +18,6 @@ namespace WiredBrainCoffee {
             InitializeComponent();
         }
 
-        private void ButtonMoveNavigation_Click(object sender, RoutedEventArgs e) {
-            //var column = (int)customerListGrid.GetValue(Grid.ColumnProperty);
-            //var newColumn = column == 0 ? 2 : 0;
-            //customerListGrid.SetValue(Grid.ColumnProperty, newColumn);
 
-            var column = Grid.GetColumn(customerListGrid);
-
-            var newColumn = column == 0 ? 2 : 0;
-
-            Grid.SetColumn(customerListGrid, newColumn);    
-        }
     }
 }
---
+++ View/CustomersView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WiredBrainCoffee.View {
+
+    public partial class CustomersView : UserControl {
+        public CustomersView() {
+            InitializeComponent();
+        }
+
+        private void ButtonMoveNavigation_Click(object sender, RoutedEventArgs e) {
+            //var column = (int)customerListGrid.GetValue(Grid.ColumnProperty);
+            //var newColumn = column == 0 ? 2 : 0;
+            //customerListGrid.SetValue(Grid.ColumnProperty, newColumn);
+
+            var column = Grid.GetColumn(customerListGrid);
+
+            var newColumn = column == 0 ? 2 : 0;
+
+            Grid.SetColumn(customerListGrid, newColumn);
+        }
+    }
+}
