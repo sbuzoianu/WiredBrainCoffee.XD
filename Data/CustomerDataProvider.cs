13b64503bfb8a2ef9d35983ac10cc45e5fe1f330
Stefan Buzoianu <stefan.buzoianu@napa.fi>
2025-01-19 21:09:16 +0200

    CustomerDataProvider class created

---
+++ Data/CustomerDataProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WiredBrainCoffee.Model;
+
+namespace WiredBrainCoffee.Data
+{
+    public interface ICustomerDataProvider {
+        Task<IEnumerable<Customer>?> GetAllAsync();
+    }
+    public class CustomerDataProvider : ICustomerDataProvider {
+        public async Task<IEnumerable<Customer>?> GetAllAsync() {
+            await Task.Delay(100); // Simulate a bit of server work
+
+            return new List<Customer>
+            {
+                new Customer{Id=1,FirstName="Julia",LastName="Developer",IsDeveloper=true},
+                new Customer{Id=2,FirstName="Alex",LastName="Rider"},
+                new Customer{Id=3, FirstName="Thomas Claudius",LastName="Huber",IsDeveloper=true},
+                new Customer{Id=4,FirstName="Anna",LastName="Rockstar"},
+                new Customer{Id=5,FirstName="Sara",LastName="Metroid"},
+                new Customer{Id=6,FirstName="Ben",LastName="Ronaldo"}
+            };
+        }
+    }
+}
