6f0c94f38013e4b639abcb5ed6458758cfc5e631
Stefan Buzoianu <stefan.buzoianu@napa.fi>
2025-02-09 20:14:46 +0200

    CustomersViewModel added

---
+++ ViewModel/CustomersViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+using WiredBrainCoffee.Data;
+using WiredBrainCoffee.Model;
+
+namespace WiredBrainCoffee.ViewModel {
+    public class CustomersViewModel
+    {
+        public CustomersViewModel(ICustomerDataProvider customerDataProvider) {
+            _customerDataProvider = customerDataProvider;
+        }
+        public ObservableCollection<Customer> Customers { get; } = new();
+        public ICustomerDataProvider _customerDataProvider { get; }
+
+        public async Task LoadAsync() {
+            if (Customers.Any()) {
+                return;
+            }
+
+            var customers = await _customerDataProvider.GetAllAsync();
+            if (customers is not null) {
+                foreach (var customer in customers) {
+                    Customers.Add(customer);
+                }
+            }
+        }
+
+    }
+}
