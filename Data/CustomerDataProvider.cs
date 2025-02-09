c26f62bfc15b11a5a296fb1afbe9cb471344bba7
Stefan Buzoianu <stefan.buzoianu@napa.fi>
2025-02-09 19:49:16 +0200

    Re-arrange elements in CustomerDataProvider

--- Data/CustomerDataProvider.cs
+++ Data/CustomerDataProvider.cs
@@ -17,11 +17,11 @@ namespace WiredBrainCoffee.Data
             return new List<Customer>
             {
                 new Customer{Id=1,FirstName="Julia",LastName="Developer",IsDeveloper=true},
-                new Customer{Id=2,FirstName="Alex",LastName="Rider"},
-                new Customer{Id=3, FirstName="Thomas Claudius",LastName="Huber",IsDeveloper=true},
-                new Customer{Id=4,FirstName="Anna",LastName="Rockstar"},
-                new Customer{Id=5,FirstName="Sara",LastName="Metroid"},
-                new Customer{Id=6,FirstName="Ben",LastName="Ronaldo"}
+                new Customer{Id=2,FirstName="Alex",LastName="Tofan"},
+                new Customer{Id=3, FirstName="Vlad",LastName="Constantinescu",IsDeveloper=true},
+                new Customer{Id=4,FirstName="Ana",LastName="Bejan"},
+                new Customer{Id=5,FirstName="Sara",LastName="Popescu"},
+                new Customer{Id=6,FirstName="Andrei",LastName="Ciobotaru"}
             };
         }
     }
